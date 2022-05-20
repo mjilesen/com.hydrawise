e9808342a49b227ef26da1ac295a90fa69ce2556
mjilesen <56786010+mjilesen@users.noreply.github.com>
2022-05-20 13:54:09 +0200

    Small fix

--- drivers/zone/device.js
+++ drivers/zone/device.js
@@ -11,12 +11,8 @@ class HydraWiseDevice extends Device {
 
   async onInit() {
     this.zoneHelper = new ZoneHelper(this.homey);
-    this.isChangedByStatusUpdate = false;
     this.registerCapabilityListener("onoff", async (value, options) => {
-            if (!this.isChangedByStatusUpdate) {
-        await this.zoneHelper.startstopZone(value, options, this);
-      }
-      this.isChangedByStatusUpdate = false;
+    await this.zoneHelper.startstopZone(value, options, this);
     });
   }
 
@@ -59,9 +55,9 @@ class HydraWiseDevice extends Device {
   async updateStatus(updatedRelay) {
     const running = updatedRelay.time === 1
     const runLength = running ? updatedRelay.run / 60 : 0;
-    this.isChangedByStatusUpdate = true;
+    //this.isChangedByStatusUpdate = true;
     this.setCapabilityValue("meter_remaining_duration", Math.ceil(runLength));
-    this.setCapabilityValue("onoff", running );
+  //  this.setCapabilityValue("onoff", running );
     this.setCapabilityValue( "meter_time_next_run_duration", running ? 0 : updatedRelay.run / 60
     );
 
